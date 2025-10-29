fea72250a06789648b63248beb5d4fbdce382fe1
Charana Sukroat <97074318+lnwwaveza007@users.noreply.github.com>
2025-10-29 20:37:01 +0700

    feat: Change default lang to thai

--- src/lib/i18n/config.ts
+++ src/lib/i18n/config.ts
@@ -2,7 +2,7 @@ export type Locale = "en" | "th";
 
 export const locales: readonly Locale[] = ["en", "th"] as const;
 
-export const defaultLocale: Locale = "en";
+export const defaultLocale: Locale = "th";
 
 export const localeCookieName = "ucan_lang";
 
